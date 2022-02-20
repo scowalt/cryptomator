e9ddbe996fe131893ab33dc9c7d5dff1a67a9813
Scott Walters <me@scowalt.com>
2022-02-19 18:04:58 -0800

    Added files

---
+++ dist/win/chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url = "https://github.com/cryptomator/cryptomator/releases/download/$env:chocolateyPackageVersion/Cryptomator-$env:chocolateyPackageVersion-x64.msi"
+$packageArgs = @{
+  packageName            = 'cryptomator'
+  fileType               = 'msi'
+  url64bit               = $url
+  checksum64             = '<!-- sha -->'
+  checksumType64         = 'sha256'
+  silentArgs             = '/qn /norestart'
+  validExitCodes         = @(0)
+  registryUninstallerKey = 'Cryptomator'
+}
+Install-ChocolateyPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+}
+else { Write-Warning "Can't find $PackageName install location" }
